523629fba5e91f99fd9bb2483615cb4ef6d704f0
MaxAref07 <makar8777@gmail.com>
2026-03-23 10:13:46 +0100

    feat(domain): Add Camera class

--- apbd-app2/apbd-app2/Domain/Models/Camera.cs
+++ apbd-app2/apbd-app2/Domain/Models/Camera.cs
@@ -2,5 +2,11 @@
 
 public class Camera : Equipment
 {
-    
+    public int MegaPixels { get; private set; }
+    public bool HasVideo { get; private set; }
+    public Camera(string name, int megaPixels, bool hasVideo) : base(name)
+    {
+        MegaPixels = megaPixels;
+        HasVideo = hasVideo;
+    }
 }
