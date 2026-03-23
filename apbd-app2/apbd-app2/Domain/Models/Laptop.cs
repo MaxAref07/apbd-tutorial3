b2e7bf46d00f1ea0bf430be9ae457309c2f2b748
MaxAref07 <makar8777@gmail.com>
2026-03-23 10:14:30 +0100

    feat(domain): Add Laptop class

--- apbd-app2/apbd-app2/Domain/Models/Laptop.cs
+++ apbd-app2/apbd-app2/Domain/Models/Laptop.cs
@@ -2,5 +2,12 @@
 
 public class Laptop : Equipment
 {
-    
+    public int RamGB { get; private set; }
+    public double ScreenSizeInch { get; private set; }
+
+    public Laptop(string name, int ramGB, double screenSizeInch) : base(name)
+    {
+        RamGB = ramGB;
+        ScreenSizeInch = screenSizeInch;
+    }
 }
