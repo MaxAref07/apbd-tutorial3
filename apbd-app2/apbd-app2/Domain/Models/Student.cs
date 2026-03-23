cda7c0997f25e66ac81f9d01350fd2906798622e
MaxAref07 <makar8777@gmail.com>
2026-03-23 10:20:57 +0100

    feat(domain): Add Student class

---
+++ apbd-app2/apbd-app2/Domain/Models/Student.cs
@@ -0,0 +1,13 @@
+namespace apbd_app2.Domain.Models;
+
+public class Student : User
+{
+    public string StudentNumber { get; private set; }
+
+    public override int MaxActiveRentals => 2;
+
+    public Student(string firstName, string lastName, string studentNumber) : base(firstName, lastName)
+    {
+        StudentNumber = studentNumber;
+    }
+}
